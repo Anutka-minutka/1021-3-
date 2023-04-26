4c4e0e591ca944d8d6e1fcdecbc842c1ef24054b
1021 <kruglakanna8@gmail.com>
2023-04-26 16:51:46 +0300

    Initial commit

--- sims.py
+++ sims.py
@@ -9,18 +9,18 @@ class Human:
         self.home=home
         self.car=car
     def get_home(self):
-        self.home=House
+        self.home=House()
     def get_job(self):
         if self.car.drive():
             pass
         else:
             self.to_repair()
             return
-        self.job=Job(job_list=)
+        self.job=Job(job_list)
     def get_car(self):
         self.car=Auto(brand_of_car)
     def eat(self):
-        if self.home.food<=0
+        if self.home.food<=0:
             self.shopping('food')
         else:
             if self.satiety>=100:
@@ -69,12 +69,74 @@ class Human:
     def to_repair(self):
         self.car.strength+=100
         self.money-=50
-    def days_indexes(self):
-        pass
+    def days_indexes(self,day):
+        day=f"Today the {day} of {self.name}'s life"
+        print(f'{day:=^50}','\n')
+        human_indexes=self.name+"'s indexes"
+        print(f"{human_indexes:^50}","\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes='Home indexes'
+        print(f'{home_indexes:^50}','\n')
+        print(f'Food - {self.home.food}')
+        print(f'Mess - {self.home.mess}')
+        car_indexes=f'{self.car.brand}car indexes'
+        print(f'{car_indexes:^50}','\n')
+        print(f'Fuel - {self.car.fuel}')
+        print(f'Strength - {self.car.strength}')
     def is_alive(self):
-        pass
-    def live(self):
-        pass
+        if self.gladness<0:
+            print('Дипресія...')
+            return False
+        elif self.satiety<0:
+            print('Голод...')
+            return False
+        elif self.money<-500:
+            print('Банкрот...')
+            return False
+    def live(self,day):
+        if self.is_alive()==False:
+            return False
+        if self.home is None:
+            print('Заселення в будинок')
+            self.get_home()
+        if self.car is None:
+            self.get_car()
+            print(f'Я купив машину {brand_of_car}')
+        if self.job is None:
+            self.get_job()
+            print(f'У мене немає роботи, я йду отримувати роботу{self.job.job}, з зарплатою {self.job.salary}')
+        self.days_indexes(day)
+        dice=random.randint(1,4)
+        if self.satiety<20:
+            print('Я йду їсти')
+            self.eat()
+        elif self.gladness<20:
+            if self.home.mess>15:
+                print('Хочеться відпочити, але в будинку безлад тому я йдe прибирати')
+                self.clean_home()
+            else:
+                print('Я відпочину')
+                self.chill()
+        elif self.money<0:
+            print('Час працювати')
+            self.work()
+        elif self.car.strength<15:
+            print('Мені потрібно відремонтувати машину')
+            self.to_repair()
+        elif dice==1:
+            print('Я відпочину')
+            self.chill()
+        elif dice==2:
+            print('Час працювати')
+            self.work()
+        elif dice==3:
+            print('я йдe прибирати')
+            self.clean_home()
+        elif dice==4:
+            print('Час смаколиків')
+            self.shopping(manage='delicacies')
 
 class Auto:
     def __init__(self, brand_list):
@@ -99,14 +161,18 @@ brand_of_car = {
         'Lada': {"fuel": 50, "strength": 40, "consumption": 10},
         'Volvo': {"fuel": 70, "strength": 150, "consumption": 8},
         'Ferari': {"fuel": 80, "strength": 120, "consumption": 14}}
-class Job:
-    def __init__(self, job_list):
-        self.job=random.choice(list(job_list))
-        self.salary=job_list[self.job]['salary']
-        self.gladness_less=job_list[self.job]['gladness_less']
 job_list={
         'Java developer':{'salary':50,'gladness_less':10},
         'Python developer':{'salary':40,'gladness_less':3},
         'C++ developer':{'salary':45,'gladness_less':25},
         'Rust developer':{'salary':70,'gladness_less':1}}
+class Job:
+    def __init__(self, job_list):
+        self.job=random.choice(list(job_list))
+        self.salary=job_list[self.job]['salary']
+        self.gladness_less=job_list[self.job]['gladness_less']
+homer_simpson=Human(name='Homer Simpson')
+for day in range(1,8):
+    if homer_simpson.live(day)==False:
+        break
 
